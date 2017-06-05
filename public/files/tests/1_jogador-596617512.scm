896eea409277309698d9632058764dc5e521db22
João Faria <jmppfaria@gmail.com>
2017-06-05 18:58:38 +0100

    dump

---
+++ public/files/tests/1_jogador-596617512.scm
@@ -0,0 +1,16 @@
+(define (isPrimeHelper x k)
+ (if (= x k)
+ #t 
+ (if (= (remainder x k) 0)    
+ #f     
+ (isPrimeHelper x (+ k 1)))))     
+ 
+ (define (printPrimesUpTo n)
+ (define result '())
+ (define (helper x)
+ (if (= x (+ 1 n))
+ result                  
+ (if (isPrime x)         
+ (cons x result) ))         
+ ( helper (+ x 1)))
+ ( helper 1 ))
